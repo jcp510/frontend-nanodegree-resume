6523f548d4518c0ab488a72a7ca6d1baadecf962
jcp510 <jcponce510@gmail.com>
2016-08-11 13:33:05 -0700

    Add grunt-respimg.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,24 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    }
-  });
+    respimg: {
+      options: {
+        // Task-specific options go here.
+        widths: [200]
+      },
+      files: [{
+          expand: true,
+          cwd: 'src/img/',
+          src: ['**.{gif,jpg,png,svg}'],
+          dest: 'build/img/'
+        }]
+      }
+    });
 
   // Load the plugin that provides the "" task.
-  grunt.loadNpmTasks('');
+  grunt.loadNpmTasks('grunt-respimg');
 
   // Default task(s).
-  grunt.registerTask('default', ['']);
+  grunt.registerTask('default', ['grunt-respimg']);
 
 };
